83a088d92b8d997bea13ef92dd9896b4a7dfb22f
Ник Николос <nic23173@gmail.com>
2021-04-26 11:10:50 +0300

    fix: add helmet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const userRout = require('./routes/users');
 const cardRout = require('./routes/cards');
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.use(helmet());
 app.use((req, _, next) => {
   req.user = {
     _id: '60841826608bf017d064edab',
