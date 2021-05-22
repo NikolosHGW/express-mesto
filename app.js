d29ee084c465e6d7676936bde8a818ddda749035
Ник Николос <nic23173@gmail.com>
2021-05-22 12:03:08 +0300

    fix: add joi custom and hex

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { celebrate, Joi, errors } = require('celebrate');
+const validator = require('validator');
 const cors = require('cors');
 const userRout = require('./routes/users');
 const cardRout = require('./routes/cards');
@@ -44,7 +45,12 @@ app.post('/signup', express.json(), celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().custom((value, helpers) => {
+      if (validator.isURL(value, { require_protocol: true })) {
+        return value;
+      }
+      return helpers.message('Невалидный url');
+    }),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
